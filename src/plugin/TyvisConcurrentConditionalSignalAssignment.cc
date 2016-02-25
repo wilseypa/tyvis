21c185e7c0af256233b3535de1d80269d0ddc1a1
Michele Castellana <michele.castellana@cern.ch>
2016-02-25 19:15:03 +0100

    Move code in the subclasses

--- src/plugin/TyvisConcurrentConditionalSignalAssignment.cc
+++ src/plugin/TyvisConcurrentConditionalSignalAssignment.cc
@@ -79,9 +79,7 @@ TyvisConcurrentConditionalSignalAssignment::_publish_cc_main(published_file& mai
   assert(dyaop);
   switch(dyaop->get_kind()) {
      case IIR_XNOR_OPERATOR:
-        main_printer.add_include("XNor.hpp", false);
-        main_printer << "XNor * " << myname << " = new XNor( \"" << myname << "\" );" << NL();
-        main_printer << "object_pointers.push_back( " << myname << " );" << NL();
+        dyaop->_publish_cc_main( main_printer );
         break;
      default:
         cerr << "ERROR! Dyadic operation " << dyaop->get_kind_text()
@@ -92,8 +90,6 @@ TyvisConcurrentConditionalSignalAssignment::_publish_cc_main(published_file& mai
    const std::string left_operand = dynamic_cast<TyvisSignalInterfaceDeclaration*>(dynamic_cast<TyvisDyadicOperator*>(_get_conditional_waveforms()->first()->get_waveform()->first()->get_value())->get_left_operand())->get_declarator()->get_text();
    const std::string right_operand = dynamic_cast<TyvisSignalInterfaceDeclaration*>(dynamic_cast<TyvisDyadicOperator*>(_get_conditional_waveforms()->first()->get_waveform()->first()->get_value())->get_right_operand())->get_declarator()->get_text();
    const std::string target = _get_target()->get_declarator()->get_text();
-   main_printer << myname << "->addSignal( \"" << left_operand << "\");" << NL();
-   main_printer << myname << "->addSignal( \"" << right_operand << "\");" << NL();
    main_printer << myname << "->addSignal( \"" << target << "\");" << NL();
    main_printer << myname << "->addOutput( \"" << target << "\"," << NL()
                 << "\"" << *current_name << "\"," << NL()
@@ -120,6 +116,7 @@ TyvisConcurrentConditionalSignalAssignment::_publish_cc( published_file &, Publi
   assert(dyaop);
   switch(dyaop->get_kind()) {
      case IIR_XNOR_OPERATOR:
+        dyaop->_publish_cc();
         break;
      default:
         cerr << "ERROR! Dyadic operation " << dyaop->get_kind_text()
@@ -128,67 +125,6 @@ TyvisConcurrentConditionalSignalAssignment::_publish_cc( published_file &, Publi
         break;
   }
 
-  published_header_file _cc_out( _get_work_library()->get_path_to_directory(), 
-				     "XNor",
-				     this );
-  _cc_out.add_include("tyvis/VHDLKernel.hh", true);
-  CC_REF( _cc_out, 
-        "TyvisConcurrentConditionalSignalAssignment::_publish_cc()" );
-  _cc_out << "class XNor : public VHDLKernel {\n"
-          << OS("public:");
-
-  // Publish the constructor
-  _cc_out << "XNor( const std::string& name );" << NL();
-
-  // The destructor.
-  _cc_out << "~XNor();\n" << NL()
-	       << "virtual std::vector<std::shared_ptr<warped::Event>> receiveEvent( const warped::Event& ) override;" << NL()
-	       << "virtual std::vector<std::shared_ptr<warped::Event>> initializeLP() override;\n" << NL()
-	       << "virtual std::vector<std::shared_ptr<warped::Event>> assignSignal( const std::string& name, int value, unsigned int delay, const VTime& timestamp ) override;\n" << NL()
-     << CS("};");
-  //Tyvis::_publish_cc_include( _cc_out, "header.hh" );
-  
-  published_cc_file _imp_out( _get_work_library()->get_path_to_directory(), 
-				     "XNor",
-				     this );
-  CC_REF( _imp_out, 
-        "TyvisConcurrentConditionalSignalAssignment::_publish_cc()" );
-
-  _imp_out.add_include("XNor.hpp", false);
-  _imp_out.add_include("tyvis/SigEvent.hh", true);
-  _imp_out.add_include("cassert", true);
-  // Publish the constructor
-  _imp_out << "XNor::XNor( const std::string& name ) : VHDLKernel(name) {}" << NL();
-
-  // The destructor.
-  _imp_out << "XNor::~XNor() {}\n" << NL()
-	       << "std::vector<std::shared_ptr<warped::Event>>\n"
-          << OS("XNor::receiveEvent( const warped::Event& event ) {")
-          << "std::cout << name_ << \": received a message from \" << event.sender_name_" << NL()
-          << "<< \" at time \" << event.timestamp() << \".\" << std::endl;" << NL()
-          << "const SigEvent sign_event = static_cast<const SigEvent&>(event);" << NL()
-          << "std::vector<std::shared_ptr<warped::Event>> response_events = assignSignal(sign_event.signalName(), sign_event.getValue(), 0, sign_event.timestamp());" << NL()
-          << "int retval = 0;" << NL()
-          << OS("for ( auto it = signals_.begin(); it != signals_.end(); it++) {")
-          << "retval++;" << NL()
-          << CS("}\n")
-          << "response_events.emplace_back(new SigEvent { (*hierarchy_.begin()->second.begin()).first, (retval % 2) ? 0 : 1, (*hierarchy_.begin()->second.begin()).second, sign_event.timestamp() });" << NL()
-          << "return response_events;" << NL()
-          << CS("};\n")
-	       << "std::vector<std::shared_ptr<warped::Event>>\n"
-	       << OS("XNor::initializeLP() {")
-          << "std::cout << name_ << \": initialization\" << std::endl;" << NL()
-          << "std::vector<std::shared_ptr<warped::Event>> response_events;" << NL()
-          << "return response_events;"
-          << CS("};\n")
-          << "std::vector<std::shared_ptr<warped::Event>>\n"
-          << OS("XNor::assignSignal( const std::string& name, int value, unsigned int delay, const VTime& timestamp ) {")
-          << "std::vector<std::shared_ptr<warped::Event>> response_events;" << NL()
-          << "assert( signals_.find(name) != signals_.end() );" << NL()
-          << "signals_[name] = value;" << NL()
-          << "return response_events;"
-          << CS("};");
-  //Tyvis::_publish_cc_include( _imp_out, "header.hh" );
   // Include all the stuff in the declarative part of process in this file
   //get_process_declarative_part()->_publish_cc( process_decls_header_file, declarations );
 }
